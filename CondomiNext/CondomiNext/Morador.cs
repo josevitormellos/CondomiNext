d462c9331983f1802496579b91e7a4c30c407bdd
Jose Vitor de Mello Silva <95095644+josevitormellos@users.noreply.github.com>
2024-04-24 16:22:11 -0300

    Back-End

---
+++ CondomiNext/CondomiNext/Morador.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondomiNext
+{
+    public class Morador : Usuario , IComparable
+    {
+        public override Servico AdicionarServico()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int AlterarServico(Servico servico)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CompareTo(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int RemoverServico(Servico servico)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Servico VerServico(string nomeServico)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pedido SolicitarServico(Servico servico)
+        {
+            return null;
+        }
+        public Avaliacao AvaliarServico(Servico servico, string comentario = null, double avaliar)
+        {
+            return null;
+        }
+        
+        public int AlterarAvaliacao (Servico servico, Avaliacao avaliacao)
+        {
+            return 1;
+        }
+        public int RemoverAvaliacao (Servico servico, Avaliacao avalicao)
+        {
+            return 1;
+        }
+    }
+}
---
+++ CondomiNext/CondomiNext/Usuario.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondomiNext
+{
+    public abstract class Usuario
+    {
+        private int id;
+        private string nome;
+        private string email;
+        private string senha;
+        private List<Servico> servicos;
+
+        public int Id { 
+            get {
+                return id;
+            } 
+            set {
+                id = value;
+            } 
+        }
+        public string Nome {
+            get { 
+                return nome;
+            } 
+            set {
+                nome = value;
+            } 
+        }
+        public string Email {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+        public string Senha {
+            get
+            {
+                return senha;
+            }
+            set
+            {
+                senha = value;
+            }
+        }
+        public List<Servico> Servicos {
+            get
+            {
+                return servicos;
+            }
+            set
+            {
+                servicos = value;
+            }
+        }
+
+        public abstract Servico AdicionarServico();
+
+        public abstract int RemoverServico(Servico servico);
+
+        public abstract int AlterarServico(Servico servico);
+
+        public abstract Servico VerServico(string nomeServico);
+    }
+}
